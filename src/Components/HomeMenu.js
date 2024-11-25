3fb469f436dde085f7ca4433f4563c197ad58a63
Lilayasuff <lyasuff@udesa.edu.ar>
2024-11-25 12:02:10 -0300

    iconos home

--- src/Components/HomeMenu.js
+++ src/Components/HomeMenu.js
@@ -4,15 +4,19 @@ import Profile from "../screens/Profile";
 import Usuarios from "../screens/Usuarios";
 import NuevoPost from "../screens/NuevoPost";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Octicons from '@expo/vector-icons/Octicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 const Tab = createBottomTabNavigator();
 
 const HomeMenu = () => {
+
   return (
-        <Tab.Navigator>
-            <Tab.Screen name= "Home" component= {Home} options={{headerShown: false}}/>
-            <Tab.Screen name= "Profile" component= {Profile} options={{headerShown: false}}/>   
-            <Tab.Screen name= "Usuarios" component= {Usuarios} options={{headerShown: false}} /> 
-            <Tab.Screen name= "NuevoPost" component= {NuevoPost} options={{headerShown: false}}/> 
+        <Tab.Navigator screenOptions= {{tabBarShowLabel: false}}> 
+            <Tab.Screen name= "Home" component= {Home} options={{tabBarIcon:() => <MaterialCommunityIcons name="home" size={24} color="black"/>}}/>
+            <Tab.Screen name= "Usuarios" component= {Usuarios} options={{tabBarIcon:() => <MaterialCommunityIcons name="account-group" size={24} color="black"/>}} /> 
+            <Tab.Screen name= "NuevoPost" component= {NuevoPost} options={{tabBarIcon:() => <Octicons name="diff-added" size={24} color="black"/>}}/> 
+            <Tab.Screen name= "Profile" component= {Profile} options={{tabBarIcon:() => <Ionicons name="person-circle" size={24} color="black"/>}}/>   
         </Tab.Navigator>
   )
 }
